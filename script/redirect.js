f00a7861055cf837b87f64d73e1aa9dbf08ae255
itsTranT1706 <khiemtrangia60@gmail.com>
2024-12-03 22:06:04 +0700

    up project

---
+++ script/redirect.js
@@ -0,0 +1,7 @@
+function myFunction(){
+    window.location.href = "gift.html";
+ }
+ //handle click open gift
+ let click = document.querySelector("p1");
+ click.addEventListener("click",myFunction);
+ // document.querySelector("#audio-player").pause();
